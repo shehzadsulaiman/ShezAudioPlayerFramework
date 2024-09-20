0bfd92f1ed202d88a2888fdee09255c736042de8
shehzadsulaiman <shehzadsulaiman@gmail.com>
2024-09-20 11:30:38 +0400

    Initial Commit

---
+++ ShezAudioPlayerFramework/ShezAudioPlayerFramework.h
@@ -0,0 +1,18 @@
+//
+//  ShezAudioPlayerFramework.h
+//  ShezAudioPlayerFramework
+//
+//  Created by sam on 20/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShezAudioPlayerFramework.
+FOUNDATION_EXPORT double ShezAudioPlayerFrameworkVersionNumber;
+
+//! Project version string for ShezAudioPlayerFramework.
+FOUNDATION_EXPORT const unsigned char ShezAudioPlayerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShezAudioPlayerFramework/PublicHeader.h>
+
+
